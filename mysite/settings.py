e7aa991b9d7888c8d3f251c9135128042b05c945
Nizam Salihin <nizamsalihin14@gmail.com>
2022-11-16 10:45:48 +0700

    add first view post_list.html

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6zcwwa_(+15pz&zc-&c^k0to=nujh5^=7h1-3h#*6zyz9xwbi)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'nizam.pythonanywhere.com']
 
 
 # Application definition
