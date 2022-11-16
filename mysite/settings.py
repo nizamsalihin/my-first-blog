ec4188430c28d4ac87508d4d5fdef0a057165be3
Nizam Salihin <nizamsalihin14@gmail.com>
2022-11-16 09:30:01 +0700

    removing *

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-6zcwwa_(+15pz&zc-&c^k0to=nujh5^=7h1-3h#*6zyz9xwbi)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
